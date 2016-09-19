224533eafbabec9421d618a7ac5cfa5e335c64f9
Thanasis Polydoros <polydorosthanasis@hotmail.com>
2016-09-19 22:35:38 +0300

    Update sudokugame.h
    Added comments to what the functions does

--- sudokugame.h
+++ sudokugame.h
@@ -2,12 +2,17 @@
 #define __SUDOKUGAME
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef struct sud *sudoku;
-sudoku initialize();
-void print(sudoku game);
-int what(int i,int j,sudoku game);
-int play(int i,int j,int x,sudoku game);
-int inrow(int i,int j,int x,sudoku game);
-int incol(int i,int j,int x,sudoku game);
-int inbox(int i,int j,int x,sudoku game);
+/*############### core functions ##############*/
+sudoku initialize(); // initializes the board
+void print(sudoku game); //prints the sudoku
+int what(int i,int j,sudoku game); //returns what there is at i,j
+int play(int i,int j,int x,sudoku game); //plays the x at i,j
+
+/*############### helpfull functions ##############*/
+int inrow(int i,int j,int x,sudoku game); //returns if x is in row i j is just to exist and be beutiful
+int incol(int i,int j,int x,sudoku game); //returns if x is in column
+int inbox(int i,int j,int x,sudoku game); //returns if x is in the 3x3 box
+
 #endif
